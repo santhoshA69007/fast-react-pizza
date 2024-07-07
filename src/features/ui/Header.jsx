5949d01a1f01217cdeb836c4acb706fc2992b834
asanthosh200210@gmail.com <asanthosh200210@gmail.com>
2024-07-07 13:12:41 +0530

    addded

---
+++ src/features/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <Header>
+            <Link to="/">Fast React Pizza Co.</Link>
+            <p>santhosh</p>
+        </Header>
+    )
+}
+
+export default Header
